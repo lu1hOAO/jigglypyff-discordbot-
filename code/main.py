de8a3de33367dd52e79a8121c352640795b399c8
luying <ring910224@gmail.com>
2022-07-03 17:41:39 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 import json
+import random
+
 with open('setting.json', mode='r',encoding='utf8') as jfile:
     jdata=json.load(jfile)
 
@@ -23,6 +25,28 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'現在延遲 {round(bot.latency*1000)} (ms)')
+
+@bot.command()
+async def 傳圖片(ctx):
+    random_pic=random.choice(jdata["pic"])
+    pic=discord.File(random_pic)
+    await ctx.send(file=pic)
+
+@bot.command()
+async def web(ctx):
+    random_pic=random.choice(jdata["url_pic"])
+    pic=discord.File(random_pic)
+    await ctx.send(random_pic)
+
+
+
+
+@bot.command()
+async def 自我介紹(ctx):
+    pic=discord.File(jdata["profile_pic"])
+    await ctx.send('哈囉我是胖丁\n想要使用指令時請打上嘿胖丁 (嘿胖丁完之後要空一格喔)')
+    await ctx.send('以下是目前指令\n ping： 目前延遲\n 傳圖片：我會傳圖片\n\n\n指令還在增加中喔！')
+    await ctx.send(file=pic)
 
 bot.run(jdata['TOKEN'])
 
