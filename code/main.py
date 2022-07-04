95b809f88a70e4ed4de103c91ca37dd24ee5e317
luying <ring910224@gmail.com>
2022-07-04 08:09:32 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import json
 import random
+import os
 
 with open('setting.json', mode='r',encoding='utf8') as jfile:
     jdata=json.load(jfile)
@@ -15,43 +16,35 @@ async def on_ready():
 @bot.event
 async def on_member_join(member):
     channel=bot.get_channel(int(jdata['Welcome_channel']))
-    await channel.send(f'{member}join!')
+    await channel.send(f'{member} join!')
 
 @bot.event
 async def on_member_remove(member):
     channel=bot.get_channel(int(jdata['Leave_channel']))
-    await channel.send(f'{member}leave!')
+    await channel.send(f'{member} leave!')
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send(f'現在延遲 {round(bot.latency*1000)} (ms)')
-
-@bot.command()
-async def 傳圖片(ctx):
-    random_pic=random.choice(jdata["pic"])
-    pic=discord.File(random_pic)
-    await ctx.send(file=pic)
-
-@bot.command()
-async def 爛笑話(ctx):
-    fun_line=random.choice(jdata["fun"])
-    await ctx.send(fun_line)
-
-@bot.command()
-async def web(ctx):
-    random_pic=random.choice(jdata["url_pic"])
-    pic=discord.File(random_pic)
-    await ctx.send(random_pic)
-
-
+async def load(ctx,extension):
+    bot.load_extension(F'cmds.{extension}')
+    await ctx.send(F'Loaded {extension} done!')
 
 
 @bot.command()
-async def 自我介紹(ctx):
-    pic=discord.File(jdata["profile_pic"])
-    await ctx.send('哈囉我是胖丁\n想要使用指令時請打上嘿胖丁 (嘿胖丁完之後要空一格喔)')
-    await ctx.send(jdata["profile"])
-    await ctx.send(file=pic)
+async def unload(ctx,extension):
+    bot.unload_extension(F'cmds.{extension}')
+    await ctx.send(F'Un-Loaded {extension} done!')
 
-bot.run(jdata['TOKEN'])
+@bot.command()
+async def reload(ctx,extension):
+    bot.reload_extension(F'cmds.{extension}')
+    await ctx.send(F'Re-Loaded {extension} done!')
+
+
+
+for filename in os.listdir('./cmds'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cmds.{filename[:-3]}')
+
+if __name__=="__main__":
+    bot.run(jdata['TOKEN'])
 
