ed866009ef77464bc25a203d7cb4c32bc549ab24
luying <ring910224@gmail.com>
2022-07-04 12:05:57 +0800

    Update react.py

--- code/cmds/react.py
+++ code/cmds/react.py
@@ -1,6 +1,7 @@
 import discord
 import random
 import json
+import datetime
 from discord.ext import commands
 from core.classes import Cog_Extension
 
@@ -19,6 +20,16 @@ class React(Cog_Extension):
     async def 爛笑話(self,ctx):
         fun_line=random.choice(jdata["fun"])
         await ctx.send(fun_line)
+
+    @commands.command()
+    async def 吃什麼(self,ctx):
+        embed=discord.Embed(title="要吃什麼", description="肚子大小事胖丁幫你決定", color=0xff4284,
+        timestamp=datetime.datetime.now())
+        embed.set_thumbnail(url="https://i.pinimg.com/474x/8e/db/c3/8edbc352639426e8146f2a658ce58662.jpg")
+        food=random.choice(jdata["food"])
+        embed.add_field(name="今天就吃", value=food, inline=False)
+        embed.set_footer(text="by juggly_puff")
+        await ctx.send(embed=embed)
 
     #@commands.command()
     #async def web(ctx):
